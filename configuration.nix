1191a521f3269f8b9e5195b3b85603a432f13bed
AwfullyBlank <maciulevicius.erikas@gmail.com>
2025-05-18 18:54:27 +0200

    removed bloat/unsed apps like obsidian steam obs

--- configuration.nix
+++ configuration.nix
@@ -10,9 +10,7 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 
 networking.hostName = "nixos"; 
-#networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-services.power-profiles-daemon.enable = true;
 
 # Enable networking
 networking.networkmanager = {
@@ -22,42 +20,25 @@ enable = true;
 #Fail2ban
 services.fail2ban = {
     enable = true;
-   # Ban IP after 5 failures
     maxretry = 5;
     ignoreIP = [
       # Whitelist some subnets
-      "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16"
-      "8.8.8.8" # whitelist a specific IP
+      #"10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16"
+      #"8.8.8.8" # whitelist a specific IP
       "nixos.wiki" # resolve the IP via DNS
     ];
-    bantime = "24h"; # Ban IPs for one day on the first ban
+    bantime = "24h";
     bantime-increment = {
       enable = true; # Enable increment of bantime after each violation
       formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
-      #multipliers = "1 2 4 8 16 32 64";
-      maxtime = "168h"; # Do not ban for more than 1 week
+      maxtime = "168h";
       overalljails = true; # Calculate the bantime based on all the violations
-    };
-    jails = {
-      #apache-nohome-iptables.settings = {
-        # Block an IP address if it accesses a non-existent
-        # home directory more than 5 times in 10 minutes,
-        # since that indicates that it's scanning.
-        #filter = "apache-nohome";
-        #action = ''iptables-multiport[name=HTTP, port="http,https"]'';
-        #logpath = "/var/log/httpd/error_log*";
-        #backend = "auto";
-        #findtime = 600;
-        #bantime  = 600;
-        #maxretry = 5;
-      #};
     };
   };
 
 #SSH
 services.openssh = {
 enable = false;
-ports = [ 22 ];
 passwordAuthentication = true;
 useDns = true;
 permitRootLogin = "no";
@@ -70,17 +51,16 @@ permitRootLogin = "no";
   };   
 
 #helps with binaries since NixOS FHS diff  
-programs.nix-ld.enable = true;
+programs.nix-ld.enable = false;
 
 boot = {
-#Bootloader.
 loader.systemd-boot.enable = true;
 loader.efi.canTouchEfiVariables = true;
 
 #ntfs
 supportedFilesystems = ["ntfs"];
 
-kernelPackages = pkgs.linuxPackages_latest;
+kernelPackages = pkgs.linuxPackages_hardened;
 
 
 #harned kernel
@@ -98,9 +78,6 @@ kernel.sysctl = {
 
 security.protectKernelImage = true;
 security.lockKernelModules = true;
-
-
-services.flatpak.enable = false;
 
 
   # Set your time zone.
@@ -141,8 +118,9 @@ hardware.graphics = {
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    jack.enable = false;
     alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa.support32Bit = false;
     pulse.enable = true; #for quickemu sound
   };
 
@@ -150,12 +128,11 @@ hardware.graphics = {
   # Configure console keymap
   console.keyMap = "no";
 
-
-  services.printing.enable = false;
-  services.pipewire.jack.enable = false;
-
-services.dbus.enable = true;
-
+services = {
+printing.enable = false;
+avahi.enable = false;
+dbus.enable = true;
+};
 
 users.users.null = {
 isNormalUser = true;
@@ -170,12 +147,7 @@ nixpkgs.config.allowUnfree = true;
 
 #packages, pkgs, PKGS
 environment.systemPackages = with pkgs; [
-qemu
-networkmanagerapplet
 quickemu
-swtpm #TPM for qemu
-OVMF #UEFI for qemu
-swaybg
 wofi
 i3blocks
 acpi
@@ -187,29 +159,26 @@ fastfetch
 wl-clipboard
 grim
 slurp
-waybar
 #term
-btop
+htop
 neovim
+fzf
+fd
 alacritty
-git
 parted
 #games
 modrinth-app
 #progs
-thunderbird
 firefox
 librewolf
-teams-for-linux
-discord-ptb
-obsidian
-davinci-resolve
+#obsidian
 #langs/lsp
 lua-language-server
 clang
 clang-tools
 lua
 jdk
+nil
 ];
 
 #fonts
@@ -217,19 +186,15 @@ fonts.packages = with pkgs; [
 nerdfonts
 ];
 
-
-#steam
-programs.steam = {
-  enable = false;
-  remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
-  dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
-  localNetworkGameTransfers.openFirewall = false; # Open ports in the firewall for Steam Local Network Game Transfers
+#git
+programs.git = {
+  enable = true;
 };
 
 
 #obs
 programs.obs-studio = {
-enable = true;
+enable = false;
 plugins = with pkgs.obs-studio-plugins; [
 obs-pipewire-audio-capture
  ];
@@ -243,18 +208,28 @@ enableCompletion = true;
 autosuggestions.enable = true;
 syntaxHighlighting.enable = true;
 promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+shellInit = ''
+cdf() {
+  cd "$(fd --type d --hidden --exclude .git | fzf)"
+}
+
+nvimf() {
+ nvim "$(fd --type f --hidden --exclude .git | fzf)"
+}
+
+fehf() {
+feh "$(fd --type f --exclude .git | fzf )"
+}
+
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  '';
 };
+
 
 #sway
 programs.sway.enable = true;
-programs.sway.wrapperFeatures.gtk = false;
-
-
-#hyprland
-programs.hyprland = {
-enable = false;
-xwayland.enable = false;
-};
+programs.sway.wrapperFeatures.gtk = true;
 
 
 environment.variables.MANPAGER = "nvim +Man!";
